ead2192d7b13c6f9f4fa64ca33c40468a2f8edb1
Desmondflexy <ikilodesmond@gmail.com>
2024-07-03 21:57:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let qrContentInput = document.getElementById("qr-content");
+    let qrGenerationForm = document.getElementById("qr-generation-form");
+    let qrCode;
+
+    function generateQrCode(qrContent) {
+        return new QRCode("qr-code", {
+            text: qrContent,
+            width: 256,
+            height: 256,
+            colorDark: "#000000",
+            colorLight: "#ffffff",
+            correctLevel: QRCode.CorrectLevel.H,
+        });
+    }
+
+    // Event listener for form submit event
+    qrGenerationForm.addEventListener("submit", e => {
+        // Prevent form submission
+        e.preventDefault();
+        let qrContent = qrContentInput.value;
+        if (!qrCode) {
+            // Generate code initially
+            qrCode = generateQrCode(qrContent);
+        } else {
+            // If code already generated then make again using same object
+            qrCode.makeCode(qrContent);
+        }
+    });
+});
